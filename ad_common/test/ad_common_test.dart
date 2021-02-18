9e561094593f67a99503cfc995603f6d2e21115e
addcn <exscall@addcn.com>
2021-02-18 10:57:50 +0800

    修改网络请求的连接和等待时长 单位

--- ad_common/test/ad_common_test.dart
+++ ad_common/test/ad_common_test.dart
@@ -1,25 +1,6 @@
-import 'package:flutter_test/flutter_test.dart';
-
+import 'package:dio/dio.dart';
 import 'package:ad_common/ad_common.dart';
 
 void main() {
-  test('千分号测试', () {
-    // String temp = '12345.934';
-    double temp = 1234567890.34;
-    String value = temp.formatNum;
-    print('test value=$value');
-  });
-
-  test('校验测试', () {
-    String temp = '校验测试a';
-
-    bool value = RegexManager.isSpecialChar(temp);
-    print('test value=$value');
-  });
-
-  test('time', () {
-    num value = 3.0;
-
-    print('test value=${value.ceil()}');
-  });
+  HttpRequest.getInstance().startAndSetRequestParams(HttpRequestSetting(logPrintInterceptor: LogPrintInterceptor(), interceptorsWrapper: InterceptorsWrapper()));
 }
