11053dfe6b4ecf23a7d4104f27308841b8f85933
addcn <exscall@addcn.com>
2021-02-24 14:22:36 +0800

    添加list 和map 常用几个方法

--- ad_common/lib/common/extension/map_extension.dart
+++ ad_common/lib/common/extension/map_extension.dart
@@ -1,7 +1,7 @@
 
 extension MapOption<K, V> on Map {
 
-  V objectForKey(K key){
+  V value(K key){
     if(containsKey(key)){
       return this[key];
     } else {
@@ -9,7 +9,10 @@ extension MapOption<K, V> on Map {
     }
   }
 
-
-
+  void set({K key, V obj}){
+    if(key == null) return;
+    if(obj == null) this.remove(key);
+    this[key] = obj;
+  }
 
 }
