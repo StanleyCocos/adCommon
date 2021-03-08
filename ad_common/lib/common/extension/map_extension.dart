97db08a373f7c5fe33c1e5fe491c1bad1c867aac
linchuanghu <linchuanghu@addcn.com>
2021-03-08 11:42:00 +0800

    修改 map 扩展方法

--- ad_common/lib/common/extension/map_extension.dart
+++ ad_common/lib/common/extension/map_extension.dart
@@ -1,20 +1,28 @@
-
 extension MapOption<K, V> on Map {
 
-  V value(K key){
-    if(containsKey(key)){
+  V value(K key) {
+    if (containsKey(key)) {
       return this[key];
     } else {
       return null;
     }
   }
 
-  void set({K key, V obj}){
-    if(key == null) return;
-    if(obj == null) {
+  void set({K key, V obj}) {
+    if (key == null) return;
+    if (obj == null) {
       this.remove(key);
     } else {
       this[key] = obj;
+    }
+  }
+
+  void add(K key, V value) {
+    if (key == null) return;
+    if (this.containsKey(key)) {
+      this.update(key, (value) => value);
+    } else {
+      this.putIfAbsent(key, () => value);
     }
   }
 }
